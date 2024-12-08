91b26fb8ceb58cde08ae7e8973ddef5379c6d8c2
gokiburiSoldier <gokiburiSoldier@163.com>
2024-12-08 17:18:08 +0800

    我干又忘加lib了

--- include/slong.h
+++ include/slong.h
@@ -1,7 +1,23 @@
+/**
+ * 如果你在同时使用`STL`和本文件时报错了: 
+ * - 先include `STL`
+ * 
+ * 例如:
+ * ```c++
+ * #include "slong.h"
+ * #include <iostream>
+ * ```
+ * 应该是:
+ * ```c++
+ * #include <iostream>
+ * #include "slong.h"
+ * ```
+ */
+
 #ifndef _SLONG_II_H
 #define _SLONG_II_H 1 
 
-#define STD_SIZE 32
+#define STD_SIZE 48
 #define byte     unsigned char
 #define mod_256 &255
 #define modwith_256 &=255
@@ -28,5 +44,7 @@ class superll {
         superll ret_subtract(superll);
         superll ret_multiply(superll);
 };
+
+void rcstr_to_ll(char*,superll*);
 
 #endif 
